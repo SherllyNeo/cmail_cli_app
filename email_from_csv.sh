c83418568c28d91578ba9c397447f152d3865f52
sherllyneo <sherlly.neomail@gmail.com>
2023-08-01 22:09:45 +0100

    changed helper bash script

--- email_from_csv.sh
+++ email_from_csv.sh
@@ -4,5 +4,6 @@ file=email_report.csv
 {
 read
 while IFS=, read -r address name ccaddress subject body attachment_path attachment_name; do
+    echo "Sending email to $name at address $address "
     mailer "$address" "$name" "$ccaddress" "$subject" "$body" "$attachment_path" "$attachment_name" >> ~/.logs/mailing.log;
   done } < $file
